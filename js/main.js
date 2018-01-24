b8e9f91c9872d6186019d0b0695fa3aab00d28d5
Yudi Utomo <yudi@codeintech.com>
2018-01-24 22:25:15 +0800

    Add Slider Events on Homepage

--- js/main.js
+++ js/main.js
@@ -239,34 +239,32 @@
 
     $(document).ready(function(){
         $('.carousel').slick({
-            dots: true,
+            dots: false,
             infinite: false,
             speed: 300,
             slidesToShow: 4,
             slidesToScroll: 4,
             responsive: [
                 {
-                breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 3,
-                        infinite: true,
-                        dots: true
-                    }
+                    breakpoint: 1200,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 3,
+                        }
                 },
                 {
-                breakpoint: 600,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2
-                    }
+                    breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2,
+                        }
                 },
                 {
-                breakpoint: 480,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
+                    breakpoint: 480,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
                 }
                 // You can unslick at a given breakpoint now by adding:
                 // settings: "unslick"
